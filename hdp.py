cd0db37d93d9bc58c69c29a292b5558a20609dd0
RoshanGaeen <117738573+RoshanGaeen@users.noreply.github.com>
2023-04-29 19:13:43 +0530

    Update hdp.py

--- hdp.py
+++ hdp.py
@@ -5,7 +5,7 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-heart_disease_model = pickle.load(open('C:/Users/siddhardhan/Desktop/Multiple Disease Prediction System/saved models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('C:\Users\pooja\OneDrive\Desktop\project','rb'))
 
 # sidebar for navigation
 with st.sidebar:
